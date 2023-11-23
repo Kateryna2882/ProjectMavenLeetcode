dfa5d26b76f87733eb5dee4886e1afc1260d86e2
Kateryna2882 <111122172+Kateryna2882@users.noreply.github.com>
2023-11-23 12:48:50 +0200

    JewelsAndStonesTest class

--- src/main/java/org/example/JewelsAndStones.java
+++ src/main/java/org/example/JewelsAndStones.java
@@ -1,2 +1,24 @@
-package org.example;public class JewelsAndStones {
+package org.example;
+
+public class JewelsAndStones {
+    public static int numJewelsInStones(String jewels, String stones) {
+        StringBuilder result = new StringBuilder();
+
+        stones.chars()
+                .filter(jewels::contains)
+                .forEach(result::append);
+
+        return result.length();
+    }
+
+    public static void main(String[] args) {
+        String jewels1 = "aA";
+        String stones1 = "aAAbbbb";
+        System.out.println(numJewelsInStones(jewels1, stones1)); // Output: 3
+
+        String jewels2 = "z";
+        String stones2 = "ZZ";
+        System.out.println(numJewelsInStones(jewels2, stones2)); // Output: 0
+    }
 }
+
